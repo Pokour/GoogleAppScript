7f5b4cfc36112660eb07bb4c80c5ab934ea4f712
Pokour <pokour@gmail.com>
2020-10-01 16:20:46 +0530

    updates

--- Lone Script/Code.js
+++ Lone Script/Code.js
@@ -1,3 +1,26 @@
+/**
+ * Hi this is a sand alone script not associated with any sheet
+ */
 function myFunction() {
-  
+ Logger.log('logging function works');
 }
+
+
+//function doPost (request) {
+// Logger.log('POST LOG');
+// return ContentService.createTextOutput('POST WORKS');
+//}
+
+function doGet(e) {
+ Logger.log('GET LOG');
+ return ContentService.createTextOutput(JSON.stringify('GET FUNCTION IS WORKING'))
+    .setMimeType(ContentService.MimeType.JAVASCRIPT);
+}
+
+
+function doPost (request) {
+  Logger.log('POST IS WORKING',request);
+//  return ContentService.createTextOutput('POST WORKS!');
+  return ContentService.createTextOutput(JSON.stringify(request))
+    .setMimeType(ContentService.MimeType.JAVASCRIPT);
+};
